77a152116f90dbc275d7ccb97d1bae5968315401
Zainab <zainabjafari998@gmail.com>
2023-01-12 14:34:39 +0100

    javascript

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const BAS_URL = ('https://fnd22-shared.azurewebsites.net/swagger/index.html')
+const BAS_URL = ('https://fnd22-shared.azurewebsites.net/swagger/v1/swagger.json')
+
+const userList = []
+
+const form = document.querySelector('.formulär')
+const button = document.querySelector('.button')
 
 
-fetch(BAS_URL)
-.then(responser => responser.json())
-.then
+const load = () => {
+    fetch(BAS_URL)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+
+
+
+
+        })
+
+}
+
+load()
+const handleSubmit = e => {
+}
+form.addEventListener('submit', handleSubmit)
+
+
+button.addEventListener('submit', e =>{
+    e.preventDefault()
+    
+})
