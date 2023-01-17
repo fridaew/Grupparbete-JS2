9b43a95551f44c51c28143610d1349a5071f18fd
Sophia Grafström <sopgra01@gmail.com>
2023-01-17 11:00:22 +0100

    javascript

--- script.js
+++ script.js
@@ -1,42 +1,66 @@
-const BAS_URL = ' https://fnd22-shared.azurewebsites.net/api/Cases'
-
+const BAS_URL = ' https://fnd22-shared.azurewebsites.net/api/Cases '
 const userList = []
-
+const lista = document.querySelector('.userLista')
 const form = document.querySelector('.formulär')
 const button = document.querySelector('.button')
 
 
 const load = () => {
-    fetch(BAS_URL)
+    fetch(BAS_URL)      //hämtar ut information från api
         .then(res => res.json())
         .then(data => {
-            console.log(data);
-
-            data.forEach(post => {
-             form.innerHTML += `
-            <article>
-            <h2>${post.email}</h2>
-            <small>${post.subject}</small>
-            <p>${post.id}</p>
-            <p>${post.created}</p>
-
-             </article>
-            `
-
-            });
-
-         
-
-
-
+      
+        data.forEach(user => {    //sparar anvöndar i min lokaka array
+          userList.unshift(user)
         })
+            listinUser()
+        
 
+            console.log(userList);
+      })
+}
+
+load()
+
+
+const listinUser = () => {
+  lista.innerHTML ='';
+
+  userList.forEach(users => {
+    const userElement = createElement(users)
+    lista.appendChild(userElement)
+  })
 }
 
 
+const createElement = (userInput) => {
 
+  let user = document.createElement('div')
+  user.classList.add('userLista')
 
-load()
+  let subject = document.createElement('p')
+  subject.classList.add('userLista-subject')
+  subject.innerText = ('Ärende: ') + userInput.subject
+
+  let email = document.createElement('p')
+  subject.classList.add('userLista-email')
+  email.innerText = ('Email: ') + userInput.email
+
+  let status = document.createElement('p')
+  status.classList.add('userLista-ärendestatus')
+  status.innerText = ('Status: ') + userInput.status.statusName
+
+  let time = document.createElement('p')
+  time.classList.add('userLista-tidpunkt')
+  time.innerText = ('Tidpunkt: ') + userInput.modified
+
+  user.appendChild(subject)
+  user.appendChild(email)
+  user.appendChild(status)
+  user.appendChild(time)
+
+  return user
+}
 
 
 const handleSubmit = e => { //skapar ett objekt som ska skickas till databasen
@@ -63,11 +87,9 @@ const handleSubmit = e => { //skapar ett objekt som ska skickas till databasen
   .then((response) => response.json())
   .then((data) => console.log(data));
 
-
+  form.reset()
+  
 }
-
-
-
 form.addEventListener('submit', handleSubmit)
 
 
